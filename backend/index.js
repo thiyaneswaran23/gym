ae370d4bceec1fdcb429c8257606f2b56aff5158
thiyaneswaran23 <145656246+thiyaneswaran23@users.noreply.github.com>
2025-02-15 06:16:04 +0530

    nigga made changes in the backend

--- backend/index.js
+++ backend/index.js
@@ -30,13 +30,11 @@ app.post('/signup', async (req, res) => {
   if (!email || !password) {
     return res.status(400).json({ error: 'Email and password are required.' });
   }
-
-  // Validate email
   if (!isValidEmail(email)) {
     return res.status(400).json({ error: 'Invalid email format.' });
   }
 
-  // Validate password
+
   if (!isValidPassword(password)) {
     return res.status(400).json({ error: 'Password must be at least 8 characters long and include one lowercase letter, one uppercase letter, one number, and one special character.' });
   }
